d87028f8002fcaa9f4e1a3627fd0fe791d6df1a8
Hamed-Taherkhani <hamedtahi7@gmail.com>
2024-02-01 15:16:21 +0330

    Fix build time error: Type error: Binding element 'searchParams' implicitly has an 'any' type.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,7 +7,14 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default async function Page({ searchParams }) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
